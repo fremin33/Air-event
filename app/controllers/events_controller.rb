c655b8fbb56c4a2d7653bd16c39105fc97e2151a
laurentdivet <laurentdivet@icloud.com>
2017-02-21 15:08:39 +0100

    event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,42 @@
 class EventsController < ApplicationController
+skip_before_action :authenticate_user!, only: [ :home]
+  def new
+    @event = Event.new
+  end
+
+  def edit
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to @event, notice: 'Event was successfully created.'
+    else
+      flash[:alert] = "invalid event"
+      render :new
+    end
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to @event, notice: 'Event was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to event_url, notice: 'Event was successfully destroyed.'
+  end
+
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit(:address, :picture, :price, :date, :place, :description,)
+    end
 end

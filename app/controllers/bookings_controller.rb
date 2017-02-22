700a75925b570834696d3c0c1a35e2602e016b37
Pierre Laroze <pierre.larozeprive@gmail.com>
2017-02-22 14:05:25 +0100

    Add booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,12 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :new ]
 
+  def new
+    @new_booking = Booking.new
+    @event = Event.find(params[:event_id])
+  end
+
+  def create
+    @new_booking = Booking.new(booking_params)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   resources :events do
-    resources :bookings, only: [:create, :edit, :update]
+    resources :bookings, only: [ :new, :create ]
   end
   mount Attachinary::Engine => "/attachinary"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

2fdc4ef4f30104a490dfbf8c8cd9e29b6dd06ea9
florian fremin <florian.fremin33@gmail.com>
2017-02-22 14:12:37 +0100

    add location in event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222113315) do
+ActiveRecord::Schema.define(version: 20170222130811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,18 +48,19 @@ ActiveRecord::Schema.define(version: 20170222113315) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.string   "address"
     t.string   "picture"
     t.integer  "price"
     t.integer  "place"
     t.datetime "date"
     t.string   "description"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "name"
     t.integer  "category_id"
+    t.integer  "localisation_id"
     t.index ["category_id"], name: "index_events_on_category_id", using: :btree
+    t.index ["localisation_id"], name: "index_events_on_localisation_id", using: :btree
     t.index ["user_id"], name: "index_events_on_user_id", using: :btree
   end
 

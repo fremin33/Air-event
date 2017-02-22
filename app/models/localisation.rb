c78489c28b8a4c14f554dc10e6355d64a4b14d1d
florian fremin <florian.fremin33@gmail.com>
2017-02-22 16:09:16 +0100

    add create new event

--- app/models/localisation.rb
+++ app/models/localisation.rb
@@ -1,4 +1,10 @@
 class Localisation < ApplicationRecord
-  geocoded_by :address
+  has_many :events
+  geocoded_by :full_street_address
   after_validation :geocode, if: :address_changed?
+
+  private
+  def full_street_address
+    "#{address} #{zip_code}, #{city}"
+  end
 end

e944b748cfeeff53ae8e468282cca2ee06201a64
florian fremin <florian.fremin33@gmail.com>
2017-02-23 23:29:12 +0100

    change structure

--- app/assets/javascripts/google_maps_autocomplete.js
+++ app/assets/javascripts/google_maps_autocomplete.js
@@ -12,20 +12,6 @@ $(document).ready(function() {
   }
 });
 
-$(document).ready(function() {
-  var event_address = $('#event_address').get(0);
-
-  if (event_address) {
-    var autocomplete = new google.maps.places.Autocomplete(event_address, { types: ['geocode'] });
-    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-    google.maps.event.addDomListener(event_address, 'keydown', function(e) {
-      if (e.keyCode == 13) {
-        e.preventDefault(); // Do not submit the form on Enter.
-      }
-    });
-  }
-});
-
 
 function onPlaceChanged() {
   var place = this.getPlace();

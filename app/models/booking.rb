ced9533a4b73a58617e9d0fdf7e5100e2521207f
Pierre Laroze <pierre.larozeprive@gmail.com>
2017-02-24 11:25:58 +0100

    fix value in booking new

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :event
-  validates :place, presence: true
+  validates :place, presence: true, :numericality => { :greater_than_or_equal_to => 1 }
 end

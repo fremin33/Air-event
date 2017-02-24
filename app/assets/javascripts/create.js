009e03c422bb219ffbd8393cd7970ff4caba6d2b
Pierre Laroze <pierre.larozeprive@gmail.com>
2017-02-24 15:24:52 +0100

    Design My booking

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -2,7 +2,8 @@ $( document ).ready(function() {
   $('#booking_place').change(function() {
     var quantity = $('#booking_place').val();
     var price = $('#booking-price').data('price');
+    var tickets = $('.booking-tickets p span').html();
     $('.booking-total-price h4 span').html(price * quantity);
-    $('.booking-tickets p span').html($('.booking-tickets p span').html() - 1);
+    $('.booking-tickets p span').html(tickets - 1);
   });
 });

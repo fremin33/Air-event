d3e708ad545feaf87ec8904dc0ec9d84dc226718
florian fremin <florian.fremin33@gmail.com>
2017-02-24 18:08:31 +0100

    fuck la societe

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,6 @@ class Event < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :bookings
-  validates :price, :date, :place, :description, presence: true
   has_attachment :picture
 
   geocoded_by :address

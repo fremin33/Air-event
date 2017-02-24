5c2a60c52cec6c33f6fffe8a4abc8e36049182f5
Pierre Laroze <pierre.larozeprive@gmail.com>
2017-02-24 14:13:32 +0100

    modify jquery

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -3,6 +3,6 @@ $( document ).ready(function() {
     var quantity = $('#booking_place').val();
     var price = $('#booking-price').data('price');
     $('.booking-total-price h4 span').html(price * quantity);
-    $('.booking-tickets p span').html($('.booking-tickets p span').html() - quantity);
+    $('.booking-tickets p span').html($('.booking-tickets p span').html() - 1);
   });
 });

225fe3859c59d5d3ce27535f7ea6c676ad5a4da2
Pierre Laroze <pierre.larozeprive@gmail.com>
2017-02-28 01:24:11 +0100

    add price

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,9 +1,11 @@
 $( document ).ready(function() {
-  $('#booking_place').change(function() {
-    var quantity = $('#booking_place').val();
-    var price = $('#booking-price').data('price');
-    var tickets = $('.booking-tickets p span').html();
-    $('.booking-total-price h4 span').html(price * quantity);
-    $('.booking-tickets p span').html(tickets - 1);
+  $("#booking_place").change(function(){
+    var quantity = $(this).val();
+    var unitprice = $('#booking-price').data('price');
+    var ticketsavailable = $('#ticketsavailable').val();
+    var totalPrice = unitprice * quantity;
+    var totalTickets = ticketsavailable - quantity;
+    $('.booking-tickets p span').html(totalTickets);
+    $('.booking-total-price h4 span').html(totalPrice)
   });
 });
